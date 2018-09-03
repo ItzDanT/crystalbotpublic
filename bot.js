c4149f6e6755dd0329caf7557cb7db8b69682221
ItzDanT <42703695+ItzDanT@users.noreply.github.com>
2018-09-03 14:32:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
 client.on("message", message => {
  if (message.content === "f!help") {
   const embed = new Discord.RichEmbed() 
